7ac3d226a97e27d9999dc8e1385c25ef4c074286
billal <proffesergio@gmail.com>
2020-09-23 13:50:48 +0600

    Updates

---
+++ precache-manifest.d4c58cd37e37bac8f528496791b897d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c2b1c177e7a742695c54f7d498e7d0",
+    "url": "/react-find-monsters/index.html"
+  },
+  {
+    "revision": "fde9e50c22ea78ef1132",
+    "url": "/react-find-monsters/static/css/main.1128c7bb.chunk.css"
+  },
+  {
+    "revision": "d366d2eb29f90101fa79",
+    "url": "/react-find-monsters/static/js/2.68474eba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-find-monsters/static/js/2.68474eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde9e50c22ea78ef1132",
+    "url": "/react-find-monsters/static/js/main.3ccb58e5.chunk.js"
+  },
+  {
+    "revision": "025ba10361f2fb8fc575",
+    "url": "/react-find-monsters/static/js/runtime-main.6bd7235c.js"
+  }
+]);
